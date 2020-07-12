fbc4fb1ada7f2a7bc489f5bc53b88c67098a0386
byron7cueva <byron7cueva@gmail.com>
2020-07-11 23:12:26 -0500

    Notification connection on server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,9 +1,40 @@
 import express from 'express';
 import http from 'http';
 import socketIO from 'socket.io';
+import { Socket } from 'socket.io';
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-server.listen(8000, () => console.log('Server is running'));
+// Collection for user cinnection
+let users: { [key: string]: string } = {};
+
+io.on('connection', (socket: Socket) => {
+  if (users[socket.id] === undefined) {
+    users[socket.id] = socket.id;
+  }
+
+  socket.emit("yourID", socket.id);
+  io.sockets.emit("allUsers", users);
+
+  // Delete usee when this is desconnected
+  socket.on('disconnect', () => {
+    delete users[socket.id];
+  });
+
+  // On call user
+  socket.on('callUser', (data: any) => {
+    io.to(data.userToCall).emit('hey',{
+      signal: data.signalData,
+      from: data.from
+    });
+  });
+
+  // On acapt call
+  socket.on('aceptCall', (data: any) => {
+    io.to(data.to).emit('callAcepted', data.signal);
+  })
+})
+
+server.listen(8000, () => console.log('Server is running')); 
